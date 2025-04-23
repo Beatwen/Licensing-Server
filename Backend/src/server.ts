c394555049973cca6494b6f5b5404c84f8976ecd
Beatwen <c_bouserez@hotmail.com>
2025-04-23 14:38:16 +0200

    Cors authorization Headers

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -87,13 +87,20 @@ const startServer = async () => {
     
     app.use(cors({
       origin: [
-        'https://licensing.noobastudio.be',  // Frontend web
-        'http://localhost:5173',             // Frontend dev
-        'http://127.0.0.1:5173'             // Frontend dev
+        'https://licensing.noobastudio.be',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173'
       ],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization', 'X-API-KEY']
+      allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-API-KEY',
+        'X-USER-KEY',
+        'access_token',
+        'authorization'
+      ]
     }));
     
     app.use(express.json());
